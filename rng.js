8b2569a3d20865d6ce0a72fc6ee0bc87156885f3
Deondre <deondre_palmer@hotmail.com>
2023-02-18 15:46:02 -0500

    v1

---
+++ rng.js
@@ -0,0 +1,54 @@
+import { boyName, girlName, lastName } from "./components/name.js";
+var checked = (document.getElementById('lock').checked); 
+let b = boyName.length;
+let g = girlName.length;
+let l = lastName.length;
+console.log(b, g, l);
+
+function names (firstName, lastName) { 
+    var checked = (document.getElementById('lock').checked);
+    const lockname = [];
+    if (checked) {
+        lockname.push(document.getElementById('lastname').innerHTML);
+        console.log(lockname);
+    }
+    if (firstName == 'boy') {
+        var given = boyName[Math.floor(Math.random()*b)];
+    } else if (firstName == 'girl') {
+        var given = girlName[Math.floor(Math.random()*g)];
+    }   
+    var surname = lastName[Math.floor(Math.random()*l)]; 
+    let display_first = (`${given}`);
+    let display_last = (`${surname}`)
+    let display = display_first + ' ' + display_last;
+    console.log(display);
+    document.getElementById('firstname').innerHTML = display_first;
+    document.getElementById('lastname').innerHTML = display_last;
+    if (checked) {
+        document.getElementById('lastname').innerHTML = lockname;
+    }
+ }
+
+// names('boy', lastName)
+$('#lock').on('click', () => {
+    var checked = (document.getElementById('lock').checked);
+    console.log(checked);
+})
+$('#boyName').on('click', () => {
+    if (checked) {
+        return (
+            names('boy', null)
+        )
+    } else {
+    return (
+        names('boy', lastName)
+    ) }
+});
+
+$('#girlName').on('click', () => {
+    return (
+        names('girl', lastName)
+    )
+});
+
+
